e52a81a183abb0940c99bb8ab0036e28a976622e
youkorr <96953496+youkorr@users.noreply.github.com>
2026-03-26 01:46:57 +0100

    Delete components/face_detction directory

--- components/face_detction/human_face_recognition_wrapper.cpp
+++
@@ -1,4 +0,0 @@
-// Wrapper to force compilation of human_face_recognition.cpp in ESPHome build
-// This ensures the implementation is compiled and linked properly
-
-#include "../../human_face_recognition/human_face_recognition.cpp"
